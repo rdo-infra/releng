0bd80a9a828aa61032455fbea2005190e8ed4048
Alfredo Moralejo <amoralej@redhat.com>
2017-09-28 17:49:36 +0200

    Add '-m' option to rdo_project
    New option '-m <CBS tag>', when used with '-r <release>' will list
    what projects doesn't have a build in  the specified CBS tag.
    
    This is useful to track missing packages when working in
    branching a new release.
    
    Change-Id: Ic693158afc9eeff3db426eec934a1491321eb8a9

--- rdoutils/rdoinfo.py
+++ rdoutils/rdoinfo.py
@@ -9,27 +9,33 @@ if not os.path.exists(local_info):
     os.makedirs(local_info)
 
 
-def get_projects(tag=None):
+def get_projects(tag=None, buildsys_tag=None):
     inforepo = rdoinfo.RdoinfoRepo(
         local_repo_path=local_info)
     inforepo.init(force_fetch=True)
-    if tag is None:
+    if tag is None and buildsys_tag is None:
         return inforepo.get_info()['packages']
     pkgs_release = []
     for package in inforepo.get_info()['packages']:
-        if tag in package['tags'].keys():
+        if tag is not None and tag in package['tags'].keys():
+            pkgs_release.append(package)
+        if (buildsys_tag is not None and 'buildsys-tags' in package.keys() and
+                buildsys_tag in package['buildsys-tags'].keys()):
             pkgs_release.append(package)
     return pkgs_release
 
 
-def get_projects_distgit(tag=None):
+def get_projects_distgit(tag=None, buildsys_tag=None):
     re_distgit = re.compile('.*?((puppet|openstack)/.*).git')
-    projects = get_projects(tag=tag)
+    projects = get_projects(tag=tag, buildsys_tag=buildsys_tag)
     distgits = []
     for project in projects:
         distgit_url = project['review-origin']
-        distgit_short = re.search(re_distgit, distgit_url).group(1)
-        distgits.append(distgit_short)
+        try:
+            distgit_short = re.search(re_distgit, distgit_url).group(1)
+            distgits.append(distgit_short)
+        except:
+            pass
     return distgits
 
 
