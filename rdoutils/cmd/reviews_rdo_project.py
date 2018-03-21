e21a05a9d70abb6c839f0ba072ee2d274833f3e7
Daniel Mellado <dmellado@redhat.com>
2018-03-21 10:33:25 +0100

    Add cliff support
    This commit adapts the review tool to use openstack cliff (Command Line
    Interface Formulation Framework). It also allows the usage of several
    formatters for the output, such as table, csv and so on.
    
    Change-Id: I64d6e45d9d05b09bbe084420acf43e25bdbd15ad

--- rdoutils/cmd/reviews_rdo_project.py
+++ rdoutils/cmd/reviews_rdo_project.py
@@ -1,29 +1,71 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import argparse
+
+import sys
+
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+from cliff.lister import Lister
+from oslo_log import log as logging
+
 from rdoutils import review_utils
 
+LOG = logging.getLogger(__name__)
+class RelEng(Lister):
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='List existing reviews for '
-                                     'project')
-    parser.add_argument('-p', '--project', dest='project',
-                        help='Project to list open reviews')
-    parser.add_argument('-s', '--status', dest='status', default='open',
-                        help='Status of the reviews to list')
-    parser.add_argument('-b', '--branch', dest='branch', default=None,
-                        help='Branch of the reviews to list')
-    return parser.parse_args()
+    def get_parser(self, prog_name):
+        parser = super(RelEng, self).get_parser(prog_name)
+        parser.add_argument('-p', '--project', dest='project',
+                            help='Project to list open reviews')
+        parser.add_argument('-s', '--status', dest='status', default='open',
+                            help='Status of the reviews to list')
+        parser.add_argument('-b', '--branch', dest='branch', default=None,
+                            help='Branch of the reviews to list')
+        return parser
+
+    def get_description(self):
+        description = (
+            'Tools to handle RDO Releng')
+        return description
+
+    def take_action(self, parsed_args):
+        LOG.info("Fetching Gerrit information")
+        client = review_utils.get_gerrit_client('rdo')
+        reviews = review_utils.get_reviews_project(client,
+                                                   parsed_args.project,
+                                                   status=parsed_args.status,
+                                                   branch=parsed_args.branch)
+        columns = reviews[0].keys()
+        data = [review.values() for review in reviews]
+        return (columns, data)
 
 
-def main():
-    args = parse_args()
-    client = review_utils.get_gerrit_client('rdo')
-    gerrit_url = 'https://review.rdoproject.org/r/#/c/'
-    reviews = review_utils.get_reviews_project(client, args.project,
-                                               status=args.status,
-                                               branch=args.branch)
-    for review in reviews:
-        print("%s %s %s %s %s %s" % (review['status'], review['_number'],
-                                     review['project'], review['subject'],
-                                     review['branch'],
-                                     gerrit_url+str(review['_number'])))
+class RelEngApp(App):
+
+    def __init__(self):
+        super(RelEngApp, self).__init__(
+            description='RelEng app',
+            version='0.1',
+            command_manager=CommandManager('review.rdo'),
+            deferred_help=True
+        )
+
+def main(argv=sys.argv[1:]):
+    myapp = RelEngApp()
+    return myapp.run(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
