18ad005e51361f623560dd43d5413ff192a6725b
Alfredo Moralejo <amoralej@redhat.com>
2018-08-07 15:57:38 +0200

    Add script to pin/unpin non-OpenStack puppet modules in rdoinfo
    One of the tasks we do for a new release is to pin/unpin non-OpenStack
    puppet modules in rdoinfo. Script pin-non_os-puppet.sh automates
    this pin using the commits in the last promoted dlrn hash repo.
    
    Top pin call:- pin-non_os-puppet.sh
    Top unpin call:- pin-non_os-puppet.sh unpin
    
    Change-Id: I35e43258af8d53c3dd853ab92b8dcb688c107a79

---
+++ scripts/new_release_scripts/pin-non_os-puppet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+RELEASE=train
+RDOINFO_DIR=/home/$USER/rdoinfo
+MODE=${1:-pin}
+
+if [ ! -d $RDOINFO_DIR ];then
+    echo "$RDOINFO_DIR doesn't exist, clone latest rdoinfo first"
+    exit 1
+fi
+
+rm -f versions.csv
+
+wget http://trunk.rdoproject.org/centos7-master/current-tripleo/versions.csv
+
+grep ^puppet versions.csv |awk -F, '{print $1 " "$2 "  " $3}'|while read pkg repo commit
+do
+    if [ $(echo $repo|grep -c openstack) -eq 0 ]; then
+        python $(dirname $0)/update-tag.py $RDOINFO_DIR $RELEASE-uc $pkg $commit $MODE
+    fi
+done
+
