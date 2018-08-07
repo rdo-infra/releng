f8b93d0d342727db6fad59eb800ce7967d3cad8d
Alfredo Moralejo <amoralej@redhat.com>
2018-08-07 15:57:38 +0200

    Add script to pin non-OpenStack puppet modules in rdoinfo
    One of the tasks we do for a new release is to pin non-OpenStack
    puppet modules in rdoinfo. Script pin-non_os-puppet.sh automates
    this pin using the commits in the last promoted dlrn hash repo.
    
    Change-Id: I35e43258af8d53c3dd853ab92b8dcb688c107a79

---
+++ scripts/new_release_scripts/update-tag.py
@@ -0,0 +1,25 @@
+import copy
+import ruamel.yaml as yaml
+import sys
+
+if len(sys.argv) != 5:
+    print("usage: update-tag.py <rdo.yml location> <tag> <package> <source-branch value>")
+    sys.exit(1)
+
+RDO_FILE = sys.argv[1]
+TAG = sys.argv[2]
+PACKAGE = sys.argv[3]
+PIN = sys.argv[4]
+
+with open(RDO_FILE, 'rb') as infile:
+    info = yaml.load(infile, Loader=yaml.RoundTripLoader)
+
+for pkg in info['packages']:
+    if pkg['project'] == PACKAGE:
+        print(TAG)
+        pkg['tags'][TAG] = { 'source-branch': PIN }
+        break
+
+with open(RDO_FILE, 'w') as outfile:
+    outfile.write(yaml.dump(info, Dumper=yaml.RoundTripDumper, indent=2))
+
