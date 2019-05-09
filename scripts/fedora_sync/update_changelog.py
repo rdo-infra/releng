d96b86a887b93e6924ba184a0ec4009526e7eec4
Alfredo Moralejo <amoralej@redhat.com>
2019-05-09 12:13:19 +0200

    Add scripts to list OpenStack packages in Fedora
    These are the scripts used to sync from Train to F32. They will be
    helpful for next imports.
    
    Change-Id: Ie6fcf83d23b421eef65d5c0aaf0457587d09ee62

---
+++ scripts/fedora_sync/update_changelog.py
@@ -0,0 +1,11 @@
+from rdopkg.utils.specfile import Spec
+
+import sys
+
+file_path = sys.argv[1]
+
+specfile = Spec(fn=file_path)
+new_version = specfile.get_vr().split('-')[0].split(':')[-1]
+chnlog_text = ("Update to upstream version %s" % new_version)
+specfile.new_changelog_entry('Alfredo Moralejo', 'amoralej@redhat.com', changes=[chnlog_text])
+specfile.save()
