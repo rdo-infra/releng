c71b92faf64cfb573601d80ecb2b24393d63f2b4
Alfredo Moralejo <amoralej@redhat.com>
2022-11-29 18:46:12 +0100

    Add option to define any gerrit server in reviews_rdo_project
    This commit is adding -g option to reviews_rdo_project so that we can
    use it for both rdo, osp (upstream openstack) or any other gerrit
    instance. Some examples:
    
    - reviews_rdo_project -p openstack/cinder-distgit (this is RDO)
    - reviews_rdo_project -p openstack/cinder-distgit -g rdo (same)
    - reviews_rdo_project -p openstack/cinder -g osp (upstream reviews)
    - reviews_rdo_project -p openstack/cinder -g https://review.opendev.org/ (upstream reviews)
    
    Change-Id: I4e9ef411c6f8b72708811c571e011c142cf985e6

--- rdoutils/cmd/reviews_rdo_project.py
+++ rdoutils/cmd/reviews_rdo_project.py
@@ -12,13 +12,20 @@ def parse_args():
                         help='Status of the reviews to list')
     parser.add_argument('-b', '--branch', dest='branch', default=None,
                         help='Branch of the reviews to list')
+    parser.add_argument('-g', '--gerrit-server', dest='gerrit_server',
+                        default='rdo',
+                        help='URL of the instance to be used (rdo if none)')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    client = review_utils.get_gerrit_client('rdo')
-    gerrit_url = 'https://review.rdoproject.org/r/#/c/'
+    if args.gerrit_server in review_utils.GERRIT_URLS.keys():
+        gerrit_url = review_utils.GERRIT_URLS[args.gerrit_server] + '/#/c/'
+    else:
+        gerrit_url = args.gerrit_server + '/#/c/'
+
+    client = review_utils.get_gerrit_client(args.gerrit_server)
     reviews = review_utils.get_reviews_project(client, args.project,
                                                status=args.status,
                                                branch=args.branch)
