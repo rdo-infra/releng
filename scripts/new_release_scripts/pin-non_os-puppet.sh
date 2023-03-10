a69ef4d357b40694109b752f25e028866eca2ed9
Joel Capitao <jcapitao@redhat.com>
2023-03-10 10:58:33 +0100

    Edit pin_puppet_modules script
    Change-Id: I7ec7f7d8f42b0332a21e78bb100f1443c1d68dfc

--- scripts/new_release_scripts/pin-non_os-puppet.sh
+++ scripts/new_release_scripts/pin-non_os-puppet.sh
@@ -7,59 +7,37 @@ BASENAME=$(basename "$0")
 TMPDIR=/tmp/${BASENAME%.*}
 RDOINFO_DIR=$TMPDIR/rdoinfo
 VERSIONS_FILE=$TMPDIR/versions.csv
-VIRTUAL_ENV=$TMPDIR/.venv
-source $DIRNAME/common.rc
+RDO_BASH_PROFILE="/etc/profile.d/rdo-profile.sh"
+
+if [ -f $RDO_BASH_PROFILE ]; then
+    source $RDO_BASH_PROFILE
+else
+    echo "This script should be run within the rdo-toolbox container image https://quay.io/repository/rdoinfra/rdo-toolbox"
+    echo "Please, refer to https://github.com/rdo-infra/releng/tree/master/container for more information."
+    exit 1
+fi
 
 MODE=${1:-pin}
 
 mkdir -p $TMPDIR
-pushd $TMPDIR >/dev/null 2>&1
+pushd $TMPDIR >/dev/null
 echo -e "Working on $TMPDIR directory"
-if [[ -d "$RDOINFO_DIR" ]]; then
-    echo "rdoinfo repository is already available"
-    pushd $RDOINFO_DIR >/dev/null 2>&1
-    git fetch origin && git rebase origin/master
-    if [[ $? -ne 0 ]]; then
-        echo "An error occured when rebasing rdoinfo repository in $RDOINFO_DIR"
-        popd >/dev/null
-        exit 1
-    fi
-    popd >/dev/null
-else
-    git clone $RDOINFO_GIT_URL $TMPDIR/rdoinfo
-    if [[ $? -eq 0 ]]; then
-        echo "rdoinfo repo is cloned"
-    else
-        echo "An error occured when cloning rdoinfo repository."
-        exit 1
-    fi
-fi
+rdo_clone_repo rdoinfo
 
-rm -f $VERSIONS_FILE
+>$VERSIONS_FILE
 curl -sL http://trunk.rdoproject.org/centos9-master/current-tripleo/versions.csv -o $VERSIONS_FILE
-
-echo -e "Creating virtualenv..."
-virtualenv -p /usr/bin/python3 $VIRTUAL_ENV >/dev/null
-source $VIRTUAL_ENV/bin/activate >/dev/null
-echo -e "Installing required modules in virtualenv..."
-python -m pip install --upgrade pip >/dev/null
-pip install distroinfo >/dev/null
-pip install git+https://github.com/rdo-infra/releng >/dev/null
-echo -e "Creating of virtualenv OK"
 
 grep ^puppet $VERSIONS_FILE |awk -F, '{print $1 " "$2 "  " $3}'|while read pkg repo commit
 do
     if [ $(echo $repo|grep -c opendev) -eq 0 ]; then
-        python $DIRNAME/update-tag.py $RDOINFO_DIR $MASTER_RELEASE-uc $pkg $commit $MODE
+        python3 $DIRNAME/update-tag.py $RDOINFO_DIR $RDO_MASTER_RELEASE-uc $pkg $commit $MODE
     fi
 done
-deactivate
 
-pushd $RDOINFO_DIR >/dev/null 2>&1
-git add tags/${MASTER_RELEASE}-uc.yml
-git commit -m "Pin non-openstack puppet modules for ${MASTER_RELEASE^}"
+pushd $RDOINFO_DIR >/dev/null
+git add tags/${RDO_MASTER_RELEASE}-uc.yml
+git commit -m "Pin non-openstack puppet modules for ${RDO_MASTER_RELEASE^}"
 popd >/dev/null
 
 echo -e "\nYou can check results in $RDOINFO_DIR"
-echo -e "If everyting's fine you can run the command: git review -t ${MASTER_RELEASE}-branching"
-
+echo -e "If everyting's fine you can run the command: git review -t ${RDO_MASTER_RELEASE}-branching"
